ed4dd92511c995c87c408b2a12613bd34293099f
Neldin Dzekovic <neldin.dzeko@gmail.com>
2015-04-22 19:36:34 +0200

    Added All meals list.

--- app/src/main/java/com/example/liquidsun/njamba/SingleRestaurantActivity.java
+++ app/src/main/java/com/example/liquidsun/njamba/SingleRestaurantActivity.java
@@ -1,11 +1,23 @@
 package com.example.liquidsun.njamba;
 
+import android.app.Service;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.example.liquidsun.njamba.service.ServiceRequest;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 
 public class SingleRestaurantActivity extends ActionBarActivity {
@@ -17,20 +29,9 @@ public class SingleRestaurantActivity extends ActionBarActivity {
 
         Intent it = getIntent();
         String nameRestaurant = it.getStringExtra("name");
-        String locationRestaurant = it.getStringExtra("location");
 
-
-
-        TextView name = (TextView)       findViewById(R.id.restaurant_single_name);
-        TextView   location= (TextView)  findViewById(R.id.restaurant_single_location);
-       // ImageView listImage = (ImageView) findViewById(R.id.image_profile_picture);
-
+        TextView name = (TextView) findViewById(R.id.restaurant_name);
         name.setText(nameRestaurant);
-        location.setText(locationRestaurant);
-
-
-
-
     }
 
 
