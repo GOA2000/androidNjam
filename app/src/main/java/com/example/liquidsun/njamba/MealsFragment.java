8804cc359d4a6480a45a59ef0e44cbbc8f10b018
GorjanGOA <kalauzovic.gorjan@gmail.com>
2015-05-07 10:37:31 +0200

    filter fragment

--- app/src/main/java/com/example/liquidsun/njamba/MealsFragment.java
+++ app/src/main/java/com/example/liquidsun/njamba/MealsFragment.java
@@ -4,6 +4,8 @@ package com.example.liquidsun.njamba;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -82,6 +84,22 @@ public class MealsFragment extends Fragment {
         });
 
         mMealFilter = (EditText) v.findViewById(R.id.edit_text_filter);
+        mMealFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ( (ArrayAdapter<Meal>)mMealsList.getAdapter()).getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         return v;
     }
